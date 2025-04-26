d2ee55ab54f7e4675571d3dae93b35755e6396c7
IsuruKalharaDeSilva <isurukalhara11@gmail.com>
2025-04-26 12:16:48 +0530

    Python Bug

--- space_adventure.py
+++ space_adventure.py
@@ -87,6 +87,7 @@ class Player(pygame.sprite.Sprite):
             self.speed_x = 8
 
         self.rect.x += self.speed_x
+
         # Keep player within the screen boundaries
         if self.rect.left < 0:
             self.rect.left = 0
